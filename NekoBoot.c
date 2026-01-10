60ddb602a809c545531642305eb1d4af19bcb5b2
Chris <morenochristopher580@gmail.com>
2026-01-10 14:19:26 -0600

    Update NekoBoot.c

--- NekoBoot.c
+++ NekoBoot.c
@@ -33,9 +33,123 @@ typedef struct {
 #pragma pack()
 
 EFI_STATUS DrawBMP(EFI_HANDLE ImageHandle, CHAR16 *FileName) {
-    // same DrawBMP as before...
-    // handles centering & scaling
-    return EFI_SUCCESS;
+    EFI_STATUS                      Status;
+    EFI_LOADED_IMAGE_PROTOCOL       *LoadedImage;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
+    EFI_FILE_HANDLE                 Volume, FileHandle;
+    EFI_GRAPHICS_OUTPUT_PROTOCOL    *GraphicsOutput;
+    UINTN                           FileSize, ReadSize;
+    UINT8                           *FileBuffer = NULL;
+    BMP_FILE_HEADER                 *BmpFileHeader;
+    BMP_INFO_HEADER                 *BmpInfoHeader;
+    UINT8                           *PixelData;
+    UINT32                          DestinationX, DestinationY;
+
+    // 1. Get the loaded image protocol to find our device
+    Status = gBS->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (VOID**)&LoadedImage);
+    if (EFI_ERROR(Status)) {
+        Print(L"Failed to get LoadedImage: %r\n", Status);
+        return Status;
+    }
+
+    // 2. Get the file system on the device where our app is loaded
+    Status = gBS->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&FileSystem);
+    if (EFI_ERROR(Status)) {
+        Print(L"Failed to get FileSystem: %r\n", Status);
+        return Status;
+    }
+
+    // 3. Open the root directory
+    Status = FileSystem->OpenVolume(FileSystem, &Volume);
+    if (EFI_ERROR(Status)) {
+        Print(L"Failed to open volume: %r\n", Status);
+        return Status;
+    }
+
+    // 4. Open the BMP file
+    Status = Volume->Open(Volume, &FileHandle, FileName, EFI_FILE_MODE_READ, 0);
+    if (EFI_ERROR(Status)) {
+        Print(L"Failed to open file %s: %r\n", FileName, Status);
+        Volume->Close(Volume);
+        return Status;
+    }
+
+    // 5. Get file size and read entire file
+    EFI_FILE_INFO *FileInfo;
+    UINTN InfoSize = sizeof(EFI_FILE_INFO) + 128;
+    FileInfo = AllocatePool(InfoSize);
+    Status = FileHandle->GetInfo(FileHandle, &gEfiFileInfoGuid, &InfoSize, FileInfo);
+    if (EFI_ERROR(Status)) {
+        Print(L"Failed to get file info: %r\n", Status);
+        goto CLEANUP_FILE;
+    }
+    FileSize = FileInfo->FileSize;
+    FreePool(FileInfo);
+
+    FileBuffer = AllocatePool(FileSize);
+    ReadSize = FileSize;
+    Status = FileHandle->Read(FileHandle, &ReadSize, FileBuffer);
+    if (EFI_ERROR(Status) || ReadSize != FileSize) {
+        Print(L"Failed to read file: %r (read %lu of %lu)\n", Status, ReadSize, FileSize);
+        goto CLEANUP_BUFFER;
+    }
+
+    // 6. Parse BMP headers
+    BmpFileHeader = (BMP_FILE_HEADER *)FileBuffer;
+    BmpInfoHeader = (BMP_INFO_HEADER *)(FileBuffer + sizeof(BMP_FILE_HEADER));
+    PixelData = FileBuffer + BmpFileHeader->bfOffBits;
+
+    // Validate BMP (basic checks)
+    if (BmpFileHeader->bfType != 0x4D42) { // "BM"
+        Print(L"Not a valid BMP file\n");
+        Status = EFI_UNSUPPORTED;
+        goto CLEANUP_BUFFER;
+    }
+    if (BmpInfoHeader->biBitCount != 24) {
+        Print(L"Only 24-bit BMP supported\n");
+        Status = EFI_UNSUPPORTED;
+        goto CLEANUP_BUFFER;
+    }
+
+    // 7. Get graphics protocol
+    Status = gBS->LocateProtocol(&gEfiGraphicsOutputProtocolGuid, NULL, (VOID**)&GraphicsOutput);
+    if (EFI_ERROR(Status)) {
+        Print(L"Failed to get GraphicsOutput: %r\n", Status);
+        goto CLEANUP_BUFFER;
+    }
+
+    // 8. Center the image on screen
+    DestinationX = (GraphicsOutput->Mode->Info->HorizontalResolution - BmpInfoHeader->biWidth) / 2;
+    DestinationY = (GraphicsOutput->Mode->Info->VerticalResolution - BmpInfoHeader->biHeight) / 2;
+
+    // 9. Draw the BMP using FrameBufferBlt
+    Status = FrameBufferBlt (
+        GraphicsOutput->Mode->FrameBufferBase,
+        GraphicsOutput->Mode->Info,
+        PixelData,
+        EfiBltBufferToVideo,
+        DestinationX,                               // DestinationX
+        DestinationY,                               // DestinationY
+        0,                                          // SourceX
+        0,                                          // SourceY
+        BmpInfoHeader->biWidth,                     // Width
+        BmpInfoHeader->biHeight,                    // Height
+        BmpInfoHeader->biWidth * 3                  // Delta (3 bytes per pixel for 24-bit)
+    );
+    if (EFI_ERROR(Status)) {
+        Print(L"FrameBufferBlt failed: %r\n", Status);
+        goto CLEANUP_BUFFER;
+    }
+
+    Print(L"BMP displayed successfully at %ux%u\n", DestinationX, DestinationY);
+    Status = EFI_SUCCESS;
+
+CLEANUP_BUFFER:
+    if (FileBuffer) FreePool(FileBuffer);
+CLEANUP_FILE:
+    FileHandle->Close(FileHandle);
+    Volume->Close(Volume);
+    return Status;
 }
 
 EFI_STATUS ChainloadWindows(EFI_HANDLE ImageHandle) {
